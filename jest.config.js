166108ebd5c8ddc923103e1cb0bac643cfaf0c74
SpireX <SpireX@outlook.com>
2023-03-01 00:34:41 +0200

    task: setup test env

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  rootDir: "./test",
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    "": ["ts-jest", {
+      isolatedModules: true,
+      tsconfig: './test/tsconfig.json',
+    }]
+  },
 };
